540fa8de268c0c0bd703600576fa2c6a04c1958c
Alexsancal <alexsancal4@gmail.com>
2025-11-10 21:01:47 -0600

    first commit

---
+++ hola.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double distPulg, distCM;
+    cout << "Cantida de pulgadas " << endl;
+    cin >> distPulg;
+
+    distCM = distPulg * 2.54;
+
+    cout << "La distancia en centimetros es " << distCM << endl;
+}
